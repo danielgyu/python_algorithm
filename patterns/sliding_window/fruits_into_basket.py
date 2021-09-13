e80bbf9512f6e06a51bf1fc5b4abbbd161c84ce0
kungyu <hismayfly@gmail.com>
2021-09-13 11:00:22 +0900

    fruits in bakset

---
+++ patterns/sliding_window/fruits_into_basket.py
@@ -0,0 +1,36 @@
+def solution(fruits: list) -> int:
+    window_start = 0 
+    max_length = 0
+    fruit_counter = {}
+
+    for window_end in range(len(fruits)):
+        fruit = fruits[window_end]
+
+        if fruit_counter.get(fruit) is None:
+            fruit_counter[fruit] = 1
+        elif fruit_counter.get(fruit):
+            fruit_counter[fruit] += 1
+
+        max_length = max(max_length, sum(fruit_counter.values()))
+ 
+        while len(fruit_counter) > 2:
+            starting_fruit = fruits[window_start]
+            
+            fruit_counter[starting_fruit] -= 1
+            if fruit_counter[starting_fruit] == 0:
+                del fruit_counter[starting_fruit]
+
+            window_start +=1
+
+
+
+    return max_length
+
+
+fruits = ['A', 'B', 'C', 'A', 'C']
+print(solution(fruits))
+assert solution(fruits) == 3
+fruits = ['A' , 'B', 'C', 'B', 'B', 'C']
+print(solution(fruits))
+assert solution(fruits) == 5
+
