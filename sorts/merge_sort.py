98c18b55ab7e62912326f8c4d137a94631432067
kungyu <hismayfly@gmail.com>
2021-11-15 21:28:43 +0900

    merge sort, rotate_array

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -24,10 +24,12 @@ def merge_sort(nums: list) -> list:
         while i < len(left):
             nums[k] = left[i]
             i += 1
+            k += 1
 
         while j < len(right):
             nums[k] = right[j]
             j += 1
+            k += 1
 
     return nums
 
@@ -36,4 +38,6 @@ def sort_array(nums: list) -> list:
 
 
 if __name__ == "__main__":
-    print(sort_array([5, 1, 1, 2, 0, 0]))
+    #print(sort_array([5, 1, 1, 2, 0, 0]))
+    #print(sort_array([-2, 3, -5]))
+    print(sort_array([-1, 2, -8, -10]))
